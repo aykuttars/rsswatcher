32285cd3fff3cf75236b5240560242c6ab8227d1
aykutt.ars <aykutt.ars@gmail.com>
2020-07-02 00:12:53 +0300

    Complete user login and jwt verification

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,make_response
 from mongoengine import *
 from PIL import Image
 from werkzeug.security import generate_password_hash as makepasswd,check_password_hash as chkpasswd
+from functools import wraps
 import datetime
+import jwt
 import uuid
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] ='y0gve&v@x8efft-+gycp(pm!l8koa_+d4uecr&bm*l49%!'
 connect('rsswatch',host='192.168.1.14', username='example', password='example', authentication_source='admin')
+app.config['MONGODB_CONNECT'] = True
+
+######################--MODELS--########################
 RANKS = (0, 0.5, 1, 1.5, 2, 2.5 ,3,3.5,4,4.5,5)
 class Users(Document):
     name         = StringField()
@@ -27,8 +32,33 @@ class RssPosts(Document):
     image        = ImageField()
     rank         = StringField(max_length=3, choices=RANKS)
     users        = ListField()
+####################--JWT-Decorator--###################
+def token_required(f):
+    @wraps(f)
+    def decorator(*args,**kwargs):
+        token =None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify({'message':'Token is required!'}),401
+        try:
+            jwt_token = jwt.decode(token,app.config['SECRET_KEY'])
+            if 'expires' in jwt_token:
+                date_time_obj = datetime.datetime.strptime(jwt_token['expires'], '%Y-%m-%d %H:%M:%S.%f')
+                diff =date_time_obj-datetime.datetime.utcnow()
+                if int(str(diff).split(':')[1])<1:
+                    return jsonify({'message':'Token is expired!'}),401
+            user = Users.objects.filter(public_id= jwt_token['id']).first()
+        except:
+            return jsonify({'message':'Token is invalid!'}),401
+        return f(user,*args,**kwargs)
+    return decorator
+###################--User-Operations--##################
 @app.route('/users',methods=['GET'])
-def get_all_users():
+@token_required
+def get_all_users(current_user):
+    if not current_user.is_admin:
+        return jsonify({'message':'you are not authorized user'})
     users = Users.objects.all()
     lst   = []
     for user in users:
@@ -43,7 +73,10 @@ def get_all_users():
 
     return jsonify({'users':lst})
 @app.route('/users/<public_id>',methods=['GET'])
-def get_one_user(public_id):
+@token_required
+def get_one_user(current_user,public_id):
+    if not current_user.is_admin:
+        return jsonify({'message':'you are not authorized user'})
     user = Users.objects.filter(public_id=public_id).first()
     if not user:
         return jsonify({'message':'No user found!'})
@@ -57,7 +90,10 @@ def get_one_user(public_id):
     return jsonify({'user':usr_dct})
     return ''
 @app.route('/users',methods=['POST'])
-def create_user():
+@token_required
+def create_user(current_user):
+    if not current_user.is_admin:
+        return jsonify({'message':'you are not authorized user'})
     data  =request.get_json()
     hashed_passwd = makepasswd(data['password'],method ='sha256')
     new_user = Users()
@@ -72,7 +108,10 @@ def create_user():
         resp = {'Message':'New User Created'}
     return jsonify(resp)
 @app.route('/users/<public_id>',methods=['PUT'])
-def update_user(public_id):
+@token_required
+def update_user(current_user,public_id):
+    if not current_user.is_admin:
+        return jsonify({'message':'you are not authorized user'})
     user = Users.objects.filter(public_id=public_id).first()
     if not user:
         return jsonify({'message':'No user found!'})
@@ -82,7 +121,10 @@ def update_user(public_id):
     else:
         return jsonify({'message':'The user has not been authorized to admin'})
 @app.route('/users/<public_id>',methods=['DELETE'])
-def delete_user(public_id):
+@token_required
+def delete_user(current_user,public_id):
+    if not current_user.is_admin:
+        return jsonify({'message':'you are not authorized user'})
     user = Users.objects.filter(public_id=public_id).first()
     if not user:
         return jsonify({'message':'No user found!'})
@@ -92,8 +134,21 @@ def delete_user(public_id):
         return jsonify({'message':'The user has been deleted'})
     else:
         return jsonify({'message':'The user has not been deleted'})
-app.config['MONGODB_CONNECT'] = True
+@app.route('/login')
+def login():
+    authentication = request.authorization
+    if not authentication or not authentication.username or not authentication.password:
+        return make_response('Please check your login method',401,{'WWW-Authenticate':'Login required'})
+    user = Users.objects.filter(username=authentication.username).first()
 
+    if not user:
+        return make_response('Please check your username',401,{'WWW-Authenticate':'Login required'})
+
+    if chkpasswd(user.passwd,authentication.password):
+        token = jwt.encode({'id':user.public_id,'expires':str(datetime.datetime.utcnow()+datetime.timedelta(minutes =30))},app.config['SECRET_KEY'],algorithm='HS256')
+        print(token)
+        return jsonify({'token':token.decode('UTF-8')})
+    return make_response('Please check your password',401,{'WWW-Authenticate':'Login required'})
 if __name__ == '__main__':
 
     app.run(debug =True)
