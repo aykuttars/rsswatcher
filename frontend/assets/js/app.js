399f21591f72eb147d0072cfd682ed8334b9965e
aykutt.ars <aykutt.ars@gmail.com>
2020-07-06 06:36:49 +0300

    Complete frontend jqeury

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -31,8 +31,6 @@ $("#loginForm").submit(function (e) {
     e.preventDefault();
     let el = $(this);
     let data = el.serializeObject();
-    console.log(data)
-    el.find('.btn-login').prop('disabled', true);
     instance.post("/login", data)
         .then(function (response) {
             el.find('.btn-login').prop('disabled', false);
@@ -49,6 +47,136 @@ $("#loginForm").submit(function (e) {
         });
 });
 
+$("#addFeedForm").submit(function (e) {
+    e.preventDefault();
+    let el = $(this);
+    let data = el.serializeObject();
+    
+    instance.post("/rss_sources", data)
+        .then(function (response) {
+            $('#addFeedModal').modal('hide')
+        })
+        .catch(function (error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: error.response.data.message
+            });
+            el.find('.btn-login').prop('disabled', false);
+        });
+});
+$("#addUserForm").submit(function (e) {
+    e.preventDefault();
+    let el = $(this);
+    let data = el.serializeObject();
+    instance.post("/users", data)
+        .then(function (response) {
+            $('#addUserModal').modal('hide')
+        })
+        .catch(function (error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: error.response.data.message
+            });
+            el.find('.btn-login').prop('disabled', false);
+        });
+});
+$("#firstUserForm").submit(function (e) {
+    e.preventDefault();
+    let el = $(this);
+    let data = el.serializeObject();
+    instance.post("/users", data)
+        .then(function (response) {
+            $('#firstUserModal').modal('hide')
+        })
+        .catch(function (error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: error.response.data.message
+            });
+            el.find('.btn-login').prop('disabled', false);
+        });
+});
+function UserList()
+{
+    let users = $('#User_list')
+    instance.get("/users")
+    .then(function (response) {
+        let body = $('#User_list')
+        if(response.data.users)
+        {
+            users.html('')
+            let list =''
+            response.data.users.forEach(function(obj,i){
+                list +=`<div class="row mb-2 mt-2">
+                        <div class="mr-2 text-capitalize">${obj.name}</div>
+                        <div class="mr-2 text-capitalize">${obj.surname}</div>
+                        <div class="btn-group" role="group">
+                            <button class="btn btn-sm btn-danger" id="usrButton${obj.id}" onclick="DeleteUser('${obj.id}')"><i class="fa fa-trash"></i> Delete</button>
+                            <button class="btn btn-sm btn-info" id="usrButton${obj.id}" onclick="GrantUser('${obj.id}')"><i class="fa fa-arrow-up"></i> Grant</button>
+                        </div>
+                </div>`
+            })
+            users.append(list)
+
+        }
+        //$('#listUsersModal').modal('hide')
+    })
+    .catch(function (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: error.response.data.message
+        });
+        el.find('.btn-login').prop('disabled', false);
+    });
+}
+function openCreateModal()
+{
+    $('#firstUserModal').modal('show')
+}
+
+function DeleteUser(id)
+{
+    instance.delete(`/users/${id}`)
+    .then(function (response) {
+        Swal.fire({
+            icon: 'info',
+            title: 'Info',
+            text: response.data.message
+        });
+    })
+    .catch(function (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: error.response.data.message
+        });
+        el.find('.btn-login').prop('disabled', false);
+    });
+}
+function GrantUser(id)
+{
+    instance.put(`/users/${id}`)
+    .then(function (response) {
+        Swal.fire({
+            icon: 'info',
+            title: 'Info',
+            text: response.data.message
+        });
+
+    })
+    .catch(function (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: error.response.data.message
+        });
+        el.find('.btn-login').prop('disabled', false);
+    });
+}
 function logout() {
     instance.delete("/login")
         .then(function (response) {
@@ -67,8 +195,19 @@ function logout() {
 
 function readDetails(el) {
     let data = JSON.parse(decodeURIComponent($(el).data("row")));
+    let image =''
+    if(data.image)
+        image = `<img class="card-img-bottom center" src="${data.image}" style="max-width:80%;max-height:80%;padding-left:20%">`
+    let card = `<div class="">
+                    <div class="card" style="width:100%">
+                    <div class="card-body">
+                        <p class="card-text">${data.detail}</p>
+                    </div>
+                    ${image}
+                    </div>
+                </div>`
     $('#detailsModalLabel').text(data.header);
-    $('#detailsModal .modal-body').html(data.detail);
+    $('#detailsModal .modal-body').html(card);
     $('#detailsModal .modal-footer').html(`
         <div>${moment(data.date).format('YYYY-MM-DD HH:mm:ss')}</div>
         ${getRank(data.id, data.rank)}
@@ -104,5 +243,10 @@ function setRank(id, rate, current = null) {
             $("#rowButton" + id).data("row", encodeURIComponent(JSON.stringify(data)));
         })
         .catch(function (error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: error.response.data.message
+            });
         });
 }
